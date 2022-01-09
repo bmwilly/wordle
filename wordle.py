fb6fef24a4ae9724bb706ade309ecea80599748c
Brandon Williams <bwilliams9@pm.me>
2022-01-09 18:30:19 +0100

    Improve printing

--- wordle.py
+++ wordle.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import List
 
 import emoji
 from nltk import FreqDist
@@ -24,15 +25,20 @@ def compare_guess(todays_letters, guess_letters):
     res = []
     for i in range(5):
         if i in greens:
-            res.append(emoji.emojize(':green_square:'))
+            res.append(emoji.emojize(":green_square:"))
         elif i in yellows:
-            res.append(emoji.emojize(':yellow_square:'))
+            res.append(emoji.emojize(":yellow_square:"))
         elif i in greys:
-            res.append(emoji.emojize(':black_large_square:'))
+            res.append(emoji.emojize(":black_large_square:"))
         else:
             raise ValueError("Something went wrong")
 
     return res
+
+
+def print_results(results: List[List[str]]):
+    for res in results:
+        print(res)
 
 
 def play():
@@ -42,7 +48,7 @@ def play():
     winner = False
 
     for _ in range(6):
-        guess = input('Your guess:')
+        guess = input("Your guess:")
         guess_letters = list(guess)
 
         if len(guess_letters) != 5:
@@ -50,15 +56,16 @@ def play():
             continue
 
         res = compare_guess(todays_letters, guess_letters)
-        print(res)
+        print("\t".join(guess_letters))
+        print("\t".join(res))
         results.append(res)
 
-        if res == [emoji.emojize(':green_square:')] * 5:
+        if res == [emoji.emojize(":green_square:")] * 5:
             print("Good job!")
-            print(results)
+            print_results(results)
             winner = True
             break
 
     if not winner:
         print(f"You lost! The correct word was '{''.join(todays_letters)}'.")
-        print(results)
+        print_results(results)
